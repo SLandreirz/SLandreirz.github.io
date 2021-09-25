bcfc0c175d0ae0fa052f99405e703fd61eebf2f6
SLandreirz <74452182+SLandreirz@users.noreply.github.com>
2021-09-25 15:46:34 +0300

    Add files via upload

---
+++ t1/javascript.js
@@ -0,0 +1,22 @@
+let p = 0;
+let age = confirm(`Чи вам э 18 рокив?`);
+let pet = prompt(`2+2`);
+let p2 = confirm(`сколько стоит машынка Hot whells? `);
+let p3 = prompt(`2*2`)
+if(age==true){
+p=p+1;
+}else{
+}
+if(pet==true){
+p=p+1
+}else{
+}
+if(p2==true){
+p=p+1
+}else{
+}
+if(p3==true){
+p=p+1 
+}else{
+}
+alert(`Вы ответили на ${p} вопроса правильно`)
